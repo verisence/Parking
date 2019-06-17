59ee06800a0dcc92f24c7fec200c9b811d5e54bf
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-06-17 15:47:25 +0300

    Add a drawable for custom edittext fields

--- app/src/main/java/com/example/parking/LoginActivity.java
+++ app/src/main/java/com/example/parking/LoginActivity.java
@@ -1,14 +1,8 @@
 package com.example.parking;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,17 +10,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
