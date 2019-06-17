c3927daa85dcad006cd7df69abede7f0b1629eab
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-06-17 16:45:08 +0300

    Add an onclick listener to the login view submit button that will submit the username to the welcome activity and open the welcome view

--- app/src/main/java/com/example/parking/LoginActivity.java
+++ app/src/main/java/com/example/parking/LoginActivity.java
@@ -2,13 +2,45 @@ package com.example.parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //declare variables
+    private Button mLoginButton;
+    private EditText mLoginUsername;
+    private EditText mLoginPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //assign variables
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mLoginUsername = (EditText) findViewById(R.id.loginUsername);
+        mLoginPassword = (EditText) findViewById(R.id.loginPassword);
+
+        mLoginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+       if (view == mLoginButton){
+           String username = mLoginUsername.getText().toString();
+           Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+
+           intent.putExtra("username", username);
+           startActivity(intent);
+
+
+       }
     }
 }
