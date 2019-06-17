df34c0797174dfac02325004da9151cc74ffb066
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-06-17 15:24:48 +0300

    Add an onclick listener to the home view button to make it open the login view

--- app/src/main/java/com/example/parking/MainActivity.java
+++ app/src/main/java/com/example/parking/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.parking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //declare button and textView
+    private Button mHomeGobutton;
+    private TextView mHomeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHomeGobutton = (Button) findViewById(R.id.homeGoButton);
+        mHomeTextView = (TextView) findViewById(R.id.homeTextView);
+
+        mHomeGobutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mHomeGobutton){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
