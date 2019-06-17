118047abd3495925326e43759de05207d774ecb2
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-06-17 20:16:12 +0300

    Add fonts to the login view

--- app/src/main/java/com/example/parking/LoginActivity.java
+++ app/src/main/java/com/example/parking/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.parking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
@@ -15,6 +17,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button mLoginButton;
     private EditText mLoginUsername;
     private EditText mLoginPassword;
+    private TextView mLoginText;
 
 
     @Override
@@ -26,6 +29,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mLoginButton = (Button) findViewById(R.id.loginButton);
         mLoginUsername = (EditText) findViewById(R.id.loginUsername);
         mLoginPassword = (EditText) findViewById(R.id.loginPassword);
+        mLoginText = (TextView) findViewById(R.id.loginText);
+
+        Typeface proximaLight = Typeface.createFromAsset(getAssets(), "fonts/light.ttf");
+        Typeface proximaMedium = Typeface.createFromAsset(getAssets(), "fonts/med.ttf");
+        Typeface proximaBold = Typeface.createFromAsset(getAssets(), "fonts/bold.ttf");
+
+        mLoginButton.setTypeface(proximaMedium);
+        mLoginText.setTypeface(proximaBold);
+        mLoginUsername.setTypeface(proximaLight);
+        mLoginPassword.setTypeface(proximaLight);
+
 
         mLoginButton.setOnClickListener(this);
 
